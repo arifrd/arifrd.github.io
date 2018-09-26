3de5595a1dd14fd65830d3b5b62f8bd29a7e7425
Arif Rudianto <arifrudianto94@gmail.com>
2018-09-26 08:47:01 +0700

    Update fade.js

--- js/fade.js
+++ js/fade.js
@@ -1,1 +1,13 @@
 
+    $(function(){
+        $('.fadein img:gt(0)').hide();
+        $('.fadein').offset({ 
+            top: $('.phone').offset().top + 38,
+            left: $('.phone').offset().left + 14 
+        });
+        setInterval(function(){
+          $('.fadein :first-child').fadeOut()
+             .next('img').fadeIn()
+             .end().appendTo('.fadein');}, 
+          4000);
+    });
